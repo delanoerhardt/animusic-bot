c49022088ddbb747d664a78bb02903a5bb5d80f3
Gabriel <gde101325@gmail.com>
2021-01-06 02:06:30 -0300

    Receives and lists the first twelve songs found

--- src/main/scala/animusic/EventHandler.scala
+++ src/main/scala/animusic/EventHandler.scala
@@ -3,13 +3,15 @@ package animusic
 import java.io.StringWriter
 
 import javax.json.Json
+import javax.json.stream.JsonParser
 
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.requests.restaction.MessageAction
 import net.dv8tion.jda.api.events.ReadyEvent
 
-import okhttp3.{OkHttpClient, RequestBody, Request, MediaType}
+import okhttp3.{OkHttpClient, RequestBody, Request, MediaType, Callback, Call, Response}
+import java.io.IOException
 
 class EventHandler(val id: String) extends ListenerAdapter {
 
@@ -31,11 +33,9 @@ class EventHandler(val id: String) extends ListenerAdapter {
       case "die" =>
         event.getJDA.shutdown()
       case anime: String =>
-        EventHandler.getBody("Vtnc")
         val request = new Request.Builder()
           .url(url)
           .header("Accept", "application/json")
-          .header("Accept-Encoding", "gzip, deflate, br")
           .header("Accept-Language", "en-US,en;q=0.9")
           .header("Connection", "keep-alive")
           .header("Content-Type", "application/json")
@@ -46,8 +46,60 @@ class EventHandler(val id: String) extends ListenerAdapter {
           .header("Sec-Fetch-Site", "cross-site")
           .header("Sec-GPC", "1")
           .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36")
-          .post(EventHandler.getBody("Vtnc"))
-        event.getChannel.sendMessage(anime).reference(event.getMessage).queue
+          .post(EventHandler.getBody(anime, 0))
+          .build()
+
+        EventHandler.client.newCall(request).enqueue(new Callback {
+          override def onFailure(call: Call, e: IOException) = {
+            e.printStackTrace()
+          }
+
+          override def onResponse(call: Call, response: Response): Unit = {
+            for(i <- (0 until response.headers.size).toList)
+              println(s"${response.headers.name(i)}: ${response.headers.value(i)}")
+
+            /* println(response.body.string())
+            response.close()
+            return */
+
+
+            val jsonParser = Json.createParser(response.body.byteStream())
+
+            var nameLinkList: List[String] = List()
+
+            var readTitleOrLink = false
+            while(jsonParser.hasNext) {
+              val event = jsonParser.next
+              event match {
+                case JsonParser.Event.KEY_NAME =>
+                  if(jsonParser.getString().equals("titles") || jsonParser.getString.equals("spotify_url"))
+                    readTitleOrLink = true
+                case JsonParser.Event.VALUE_STRING if(readTitleOrLink) =>
+                  readTitleOrLink = false
+                  println(jsonParser.getString())
+                  nameLinkList = nameLinkList :+ (jsonParser.getString)
+                case _ =>
+              }
+            }
+
+            var finalMessage = ""
+            var previousWasLink = true
+            nameLinkList.foreach((el) => {
+              if(!el.startsWith("https://open.spotify.com/track/")) {
+                previousWasLink = false
+                finalMessage += el
+              }
+              else if(!previousWasLink) {
+                previousWasLink = true
+                finalMessage += s"\t\t${el}\n"
+              }
+            })
+            println(finalMessage)
+            event.getChannel.sendMessage(finalMessage).reference(event.getMessage).queue
+
+            response.close()
+          }
+        })
     }
   }
 }
@@ -57,14 +109,23 @@ object EventHandler {
 
   private val MEDIA_TYPE_JSON = MediaType.get("application/json; charset=utf-8")
 
-  def getBody(x: String): RequestBody = {
-    //{"requests": [{"indexName": "songs_prod","params": "query=one%20piece&hitsPerPage=12&maxValuesPerFacet=100&page=0&highlightPreTag=__ais-highlight__&highlightPostTag=__%2Fais-highlight__&facets=%5B%22song_type%22%2C%22markets%22%2C%22season%22%2C%22label%22%5D&tagFilters="}]}
+  def getBody(name: String, page: Int): RequestBody = {
     val writer = new StringWriter
-    val gen = Json.createGenerator(writer)
-    gen.writeStartObject().writeEnd().close()
-    println(writer.toString())
+    val jsonGenerator = Json.createGenerator(writer)
+    jsonGenerator
+      .writeStartObject()
+        .writeStartArray("requests")
+          .writeStartObject()
+            .write("indexName", "songs_prod")
+            .write("params", s"query=${name.replace(" ", "%20")}&hitsPerPage=12&maxValuesPerFacet=100&page=${page}&highlightPreTag=__ais-highlight__&highlightPostTag=__%2Fais-highlight__&facets=%5B%22song_type%22%2C%22markets%22%2C%22season%22%2C%22label%22%5D&tagFilters=")
+          .writeEnd()
+        .writeEnd()
+      .writeEnd().close()
+    
+    val jsonString = writer.toString()
 
+    writer.close()
 
-    RequestBody.create(this.MEDIA_TYPE_JSON, writer.toString())
+    RequestBody.create(this.MEDIA_TYPE_JSON, jsonString)
   }
 }
